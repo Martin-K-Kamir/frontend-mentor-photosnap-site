698cd8d1fb8ce873ba00300990c9aa7938fbc1ff
Martin Kamir <martin.k.kamir@gmail.com>
2023-01-10 09:01:23 +0100

    w

--- src/react/components/Cta.jsx
+++ src/react/components/Cta.jsx
@@ -4,10 +4,11 @@ import Button from "./Button";
 export default function Cta() {
     return (
         <div className="[ stacked ] [ fg-neutral-1 bg-neutral-5 ]" data-extrinsic-sizing='true'>
+            <span className="pattern-line" style={{"--_length" : "100%"}} aria-hidden="true"></span>
             <Image dir="shared" name="cta" widths={["1440", "768", "375"]}/>
             <div className="[ flow wrapper ] [ align-items-center direction-row//md ]">
-                <h2 className="title-5">We’re in beta. Get your invite today!</h2>
-                <Button to="/coming-soon" type="link">Get an invite</Button>
+                <h2 className="[ title-5 ] [ measure-2 ]">We’re in beta. Get your invite today!</h2>
+                <Button to="/coming-soon" type="link" utils="justify-self-start justify-self-end//md">Get an invite</Button>
             </div>
         </div>
     );
--- src/react/components/Split.jsx
+++ src/react/components/Split.jsx
@@ -14,11 +14,11 @@ export default function Split(props) {
 
     return (
         <div className="[ split ] [ direction-row//md ]">
-            <div className="[ split__content ] [ stack ] [ justify-center//md justify-self-center//md align-content-center//md ]">
+            <div className="[ split__content ] [ stack ] [ measure-3 justify-center//md justify-self-center//md align-content-center//md ]">
                 {props.hero && <span className="[ pattern-line ] [ hide//md ]" data-direction="horizontal" style={{"--_length" : "30%", "--_block-start" : "0"}} aria-hidden="true"></span>}
-                <Title className="title-5">{props.title}</Title>
-                <p className={`[ ${props.hero ? "fg-neutral-3" : "fg-neutral-4"} ]`}>{props.desc}</p>
-                {props.btn && <Button type="link" {...props.btn}>{props.btn.content}</Button>}
+                <Title className={`[ title-5 ] [ ${props.hero ? "measure-2" : "measure-1"} ]`}>{props.title}</Title>
+                <p className={`${props.hero ? "fg-neutral-3" : "fg-neutral-4"}`}>{props.desc}</p>
+                {props.btn && <Button type="link" utils="justify-self-start" {...props.btn}>{props.btn.content}</Button>}
             </div>
             <Image {...props.image}/>
         </div>
