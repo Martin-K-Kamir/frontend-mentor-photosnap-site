b27749030caf0123256ad78939a1e04ee27e2f38
Martin Kamir <martin.k.kamir@gmail.com>
2023-01-11 23:27:23 +0100

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = () => {
     return {
         plugins: [
-            // require('postcss-preset-env'),
+            require('postcss-preset-env'),
             // require('postcss-variable-compress')(
             //     ['--_block-start', '--_block-end', '--_inline-start', '--_inline-end', '--_translate-x', '--_translate-y', '--_inline-size', "--_thickness", "--_length"]
             // ),
