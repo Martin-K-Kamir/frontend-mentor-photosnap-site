f6ab2d07d7a88dc0020d94b8ccda4b002568831c
Martin Kamir <martin.k.kamir@gmail.com>
2023-01-11 23:28:51 +0100

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -4,11 +4,11 @@ module.exports = () => {
     return {
         plugins: [
             require('postcss-preset-env'),
-            // require('postcss-variable-compress')(
-            //     ['--_block-start', '--_block-end', '--_inline-start', '--_inline-end', '--_translate-x', '--_translate-y', '--_inline-size', "--_thickness", "--_length"]
-            // ),
+            require('postcss-variable-compress')(
+                ['--_block-start', '--_block-end', '--_inline-start', '--_inline-end', '--_translate-x', '--_translate-y', '--_inline-size', "--_thickness", "--_length"]
+            ),
             // require("postcss-combine-media-query"),
-            // require("postcss-combine-duplicated-selectors"),
+            require("postcss-combine-duplicated-selectors"),
         ],
     };
 };
